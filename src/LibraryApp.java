9ad51e1bdc7bf8d2dc43a927ee6305deab4f0eb8
wilder898 <wilder898@gmail.com>
2025-06-17 19:34:11 -0500

    feat: implement basic LibraryApp structure with imports and main method

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import com.sena.app.models.Book;
+
 public class LibraryApp {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private static Scanner sacanner = new Scanner(System.in);
+    private static List<Book> library = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("🚀 Biblioteca Digital - Versión 1.0");
+
+        sacanner.close();
     }
 }
